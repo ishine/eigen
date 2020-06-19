0b5c28f7c3f201f20bfa711e07fd5c7c8f28faee
asus <744984949@qq.com>
2020-06-19 22:15:52 +0800

    assembly.asm added.

--- src/std/lagacy.cpp
+++ src/std/lagacy.cpp
@@ -14,40 +14,40 @@ double gelu(double x) {
 	return x * cdf;
 }
 
-double hard_sigmoid(double x) {
-	if (x < -2.5)
-		return 0;
-	if (x > 2.5)
-		return 1;
-	return 0.2 * x + 0.5;
-}
-
-double relu(double x) {
-	return x < 0 ? 0 : x;
-}
-
-void stosd(void *edi, dword eax, qword rcx) {
-	while (rcx) {
-		*(int*) edi = eax;
-		--rcx;
-	}
-}
-
-void* movsq(void *_edi, const void *_esi, qword rcx) {
-	qword *edi = (qword*) _edi;
-	qword *esi = (qword*) _esi;
-	while (rcx) {
-		*edi = *esi;
-		--rcx;
-		++edi;
-		++esi;
-	}
-	return edi;
-}
-
-double zero = 0;
-double one = 1;
-double one_fifth = 0.2;
-double half = 0.5;
+//double hard_sigmoid(double x) {
+//	if (x < -2.5)
+//		return 0;
+//	if (x > 2.5)
+//		return 1;
+//	return 0.2 * x + 0.5;
+//}
+//
+//double relu(double x) {
+//	return x < 0 ? 0 : x;
+//}
+//
+//void stosd(void *edi, dword eax, qword rcx) {
+//	while (rcx) {
+//		*(int*) edi = eax;
+//		--rcx;
+//	}
+//}
+//
+//void* movsq(void *_edi, const void *_esi, qword rcx) {
+//	qword *edi = (qword*) _edi;
+//	qword *esi = (qword*) _esi;
+//	while (rcx) {
+//		*edi = *esi;
+//		--rcx;
+//		++edi;
+//		++esi;
+//	}
+//	return edi;
+//}
+//
+//double zero = 0;
+//double one = 1;
+//double one_fifth = 0.2;
+//double half = 0.5;
 }
 
