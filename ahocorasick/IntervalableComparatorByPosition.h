ff2787dbaa6293ca0902db06d9cea6182d129131
Lizhi Zhou <zhoulizhi@patsnap.com>
2020-06-29 11:33:55 +0800

    ahocorasick added

--- ahocorasick/IntervalableComparatorByPosition.h
+++
@@ -1,8 +0,0 @@
-class IntervalableComparatorByPosition {
-
-public:
-	int compare(Intervalable intervalable, Intervalable intervalable2) {
-		return intervalable.getStart() - intervalable2.getStart();
-	}
-
-};
